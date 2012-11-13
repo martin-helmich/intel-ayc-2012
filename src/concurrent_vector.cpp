ef315e092466f56195677d4c1f128a5438169b4b
Martin Helmich <martin.helmich@hs-osnabrueck.de>
2012-11-13 11:28:32 +0100

    Angefahgen, eigenen concurrent_vector zu bauen.

---
+++ src/concurrent_vector.cpp
@@ -0,0 +1,18 @@
+/*
+ * concurrent_vector.cpp
+ *
+ *  Created on: 13.11.2012
+ *      Author: mahelmic
+ */
+
+#include <vector>
+
+namespace oma
+{
+
+class concurrent_vector : public std::vector
+{
+
+}
+
+}
